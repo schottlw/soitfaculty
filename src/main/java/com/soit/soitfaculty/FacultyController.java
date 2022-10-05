72745fa55bace4cc1f897f4f451392d5819d118d
schottlw <55925342+schottlw@users.noreply.github.com>
2022-10-05 14:03:29 -0400

    Data loaded

---
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -0,0 +1,33 @@
+package com.soit.soitfaculty;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.soit.soitfaculty.model.Faculty;
+
+@Controller
+@RequestMapping("/Faculties")
+public class FacultyController {
+	
+	// Upload Faculty info
+	private List<Faculty> theFaculties;
+	
+	@PostConstruct
+	private void loadData() {
+		// Create Faculties
+		Faculty fac1 = new Faculty(1, "Kelly", "Miller", "Kelly@uc.edu");
+		Faculty fac2 = new Faculty(2, "Robert", "Lee", "Robert@uc.edu");
+		Faculty fac3 = new Faculty(3, "Laura", "West", "Laura@uc.edu");
+		
+		//Create our List
+		theFaculties = new ArrayList();
+		
+		//Add to our List
+		theFaculties.add(fac1);
+		theFaculties.add(fac2);
+		theFaculties.add(fac3);
+	}
+	
+	//Mapping for "/list"
+}
