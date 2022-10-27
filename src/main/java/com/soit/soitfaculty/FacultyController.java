a95c2997eb85a929e88318e479a4a8634c687b27
schottlw <55925342+schottlw@users.noreply.github.com>
2022-10-27 14:50:36 -0400

    set faculty entitiy

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.soit.soitfaculty.model.Faculty;
+
+import com.soit.soitfaculty.entity.Faculty;
 
 @Controller
 @RequestMapping("/Faculties")
--- src/main/java/com/soit/soitfaculty/entity/Faculty.java
+++ src/main/java/com/soit/soitfaculty/entity/Faculty.java
@@ -1,10 +1,29 @@
-package com.soit.soitfaculty.model;
+package com.soit.soitfaculty.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="faculty")
 public class Faculty {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	@Column(name="id")
 	private int id;
+	
+	@Column(name="first_name")
 	private String firstName;
+	
+	@Column(name="last_name")
 	private String lastName;
+	
+	@Column(name="email")
 	private String email;
 	
 	public Faculty() {
