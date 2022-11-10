29d9c75f2b2897c6413ce28cf171e4967fc181b5
schottlw <55925342+schottlw@users.noreply.github.com>
2022-11-10 15:12:42 -0500

    Display last names in order, index.html configured

--- src/main/java/com/soit/soitfaculty/dao/FacultyRepository.java
+++ src/main/java/com/soit/soitfaculty/dao/FacultyRepository.java
@@ -1,9 +1,10 @@
 package com.soit.soitfaculty.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.soit.soitfaculty.entity.Faculty;
 
 public interface FacultyRepository extends JpaRepository<Faculty, Integer> {
+	public List<Faculty>findAllByOrderByLastName();
 
 }
